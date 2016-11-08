9963003cc75606e1e8a4bc0d6d9e1d92665d4104
Herro-Sama <sandylily66@hotmail.com>
2016-11-08 10:33:17 +0000

    Day 345: Actually made some progress...

--- Collosi_Arena/Main.py
+++ Collosi_Arena/Main.py
@@ -14,17 +14,22 @@ pygame.init()
 
 Screen = pygame.display.set_mode((ScreenWidth,ScreenHeight),pygame.FULLSCREEN, 32)
 pygame.display.set_caption('Collosi Arena')
-pygame.image.load('Heath Bar.PNG')
-Screen.blit('Health Bar.PNG,
+class Sprite:
+    def __init__(self):
+        self.BackgroundImage = pygame.image.load('Background.jpg')
+
+    def Update(self):
+        Screen.blit(self.BackgroundImage, (0, 0))
 class Player:
     def __init__(self):
         self.PosX = 200
         self.PosY = 600
         self.Health = 5
-        self.image = pygame.image.load('CharacterPlaceholder.jpg')
+        self.PlayerSprite = pygame.image.load('CharacterPlaceholder.jpg')
 
     def Update(self):
-        Screen.blit(self.image, (self.PosX, self.PosY))
+        Screen.blit(self.PlayerSprite, (self.PosX, self.PosY))
+
 
 class AI:
     def __init__(self):
@@ -33,7 +38,7 @@ class AI:
         self.Health = 100
         self.Attacking = False
         self.image = pygame.image.load('Golem.gif')
-        Screen.blit(self.image, (self.PosX, self.PosY))
+
 
 
     def AIMovement (self,PlayerPosition):
@@ -48,16 +53,16 @@ class AI:
         self.Attacking = True
 
     def Update(self):
+        self.HealthBar = pygame.image.load('HealthBar.png')
         Screen.blit(self.image, (self.PosX, self.PosY))
+        Screen.blit(self.HealthBar, (self.PosX, self.PosY))
 
 
-
-
+sprite = Sprite()
 player = Player()
 Golem = AI()
 
 while True:
-    Screen.blit (Background,(0,0))
     if Golem.Attacking == False:
        Counter += 1
     keys_pressed = pygame.key.get_pressed()
@@ -71,8 +76,9 @@ while True:
     if keys_pressed[K_d]:
         player.PosX += 3
     print Golem.Attacking
-    Golem.Update()
+    sprite.Update()
     player.Update()
+    Golem.Update()
 
 
 
